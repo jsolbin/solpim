cf5f1f26466a7dce1f357b136caa228b94033dc9
jsolbin <jsolbin0724@gmail.com>
2026-03-22 14:18:22 +0000

    chore: set up global theme and font setup

---
+++ src/styles/font.ts
@@ -0,0 +1,13 @@
+const systemSans = ['system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'sans-serif'] 
+
+export const fonts = {
+  body: ['Inter', ...systemSans].join(', '),
+  heading: ['Montserrat', ...systemSans].join(', '),
+}
+
+export const fontWeights = {
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+}
